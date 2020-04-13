93a52b34bdcdd9095258e582b412a27f57089908
Boldoosang <jbaldeo@yahoo.com>
2020-04-13 13:30:51 -0400

    Created classes for different statistics

--- public/coronaTracker.js
+++ public/coronaTracker.js
@@ -2,11 +2,12 @@ let stats = "https://pomber.github.io/covid19/timeseries.json";
 let countryList = "https://raw.githubusercontent.com/pomber/covid19/master/docs/countries.json";
 let results;
 let countries;
-let coronaTrackerArea = document.querySelector("#coronaTracker");
+let coronaTrackerArea = document.querySelector(".coronaTracker");
 
 //TESTING
 let userSelection = "Trinidad and Tobago";
 
+//GET CORONA RESULTS AND CORONA COUNTRIES
 async function getCoronaData(stats){
     try {
         let response = await fetch(stats);
@@ -25,5 +26,24 @@ async function getCoronaData(stats){
 }
 
 getCoronaData(stats);
-
 console.log(results);
+
+
+//END GET CORONA RESULTS AND CORONA COUNTRIES
+
+//CORONA RESULT ELEMENTS
+let cTableElement = coronaTrackerArea.querySelector(".coronaTableCountry");
+let cPieElement = coronaTrackerArea.querySelector(".coronaPieChart");
+let cMapWorld = coronaTrackerArea.querySelector(".coronaMapWorld");
+let cMapRegion = coronaTrackerArea.querySelector(".coronaMapRegion");
+
+console.log(coronaTrackerArea);
+console.log(cTableElement);
+console.log(cPieElement);
+console.log(cMapWorld);
+console.log(cMapRegion);
+
+//END CORONA RESULT ELEMENTS
+
+cTableElement.innerHTML = "<p>Hello</p>"
+
