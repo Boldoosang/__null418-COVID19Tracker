8f22c8f8e9278c8c2b646d74c603c7f44c638384
thehuman <tevon.thatcher@my.uwi.edu>
2020-04-14 11:34:23 -0400

    Added dropdown animation

--- public/script.js
+++ public/script.js
@@ -4,6 +4,11 @@
     var instances = M.Sidenav.init(elems, {});
   });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {});
+  });
+
   
   async function navigate(title, url){
     document.title = title;
