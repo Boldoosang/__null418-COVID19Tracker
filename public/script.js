b9999f34aab7aeeec38848c0a99c7b9364249c5e
Boldoosang <jbaldeo@yahoo.com>
2020-04-16 15:16:35 -0400

    Changed style of nav logo

--- public/script.js
+++ public/script.js
@@ -3,10 +3,9 @@
     var instances = M.Sidenav.init(elems, {});
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {});
-  });
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+    });
 
   async function navigate(title, url){
     document.title = "__null418 COVID19 - " + title;
