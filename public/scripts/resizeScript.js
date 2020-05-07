20f14d75a5b4c066715afe3e24acbfd3d7d4fdf1
Boldoosang <jbaldeo@yahoo.com>
2020-05-07 05:45:07 -0400

    Updated scalability of tracker on mobile

--- public/scripts/resizeScript.js
+++ public/scripts/resizeScript.js
@@ -1,6 +1,6 @@
   function iframeLoaded() {
     let iFrameID = document.getElementById('iFrame1'); //load iframe into object
     if(iFrameID) {
-      iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + 50 + "px"; //change the iframe height to the total size of the inner page
+      iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + 250 + "px"; //change the iframe height to the total size of the inner page
       }   
   } 
